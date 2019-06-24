b0e1900c02d16b67ceaa106e11f0497813b4727f
ishammohamed <mail.to.isham@gmail.com>
2019-06-24 14:35:19 +0530

    tow iteraion method

--- over lapping boxes.py
+++ over lapping boxes.py
@@ -7,25 +7,12 @@ for k in l:
 	boxes.append(set([(i,j) for i in range (k[0],k[2]) for j in range(k[1],k[3])]))
 print(*boxes,sep="\n")
 common=[]
-
-for i in range(len(boxes)+1):
-	for j in range(i+1,len(boxes)):
-		if len(boxes[i].intersection(boxes[j]))>0 :
-			temp=boxes[i].intersection(boxes[j])
-			common.append((len(temp),l[i][4]+l[j][4]))
-			settemp=[i,j]
-			for k in range(len(boxes)):
-				if k not in settemp:
-					if temp.intersection(boxes[k])>0:
-						x=temp.intersection(boxes[k])
-						if x in common:
-							y=common.index(x)
-							common[y][1]=common[y][2]+l[k][4]
-							common[y][0]+=len(temp)
-						else:
-							common.append(len(x),)
+def findcommon(l,area,val,cb):
+	for i in boxes:
+		test=i.itersec
+		for i boxes
+		if i.intersection()
 
 
-
-
+findcommon()
 print(common)
