888a9c2abeb2fdf5948af3ceb13d1b6708737249
ishammohamed <mail.to.isham@gmail.com>
2019-06-24 22:45:25 +0530

    bad permutations skelton

--- Bad Permutations.py
+++ Bad Permutations.py
@@ -1,16 +1,25 @@
 #Bad Permutations
 from itertools import permutations
 #n_testcases=int(input())
-l=[9]
-#for _ in range(n_testcases):
+l=[6,9]
+# for _ in range(n_testcases):
 #	l.append(int(input()))
 factors=[]
 for i in l:
 	factors.clear()
-	factors=[0,1]
-	for j in range (2,i//2):
+	factors=[1]
+	for j in range (2,i//2+1):
 		if i%j==0:
 			factors.append(j)
+	factors.append(i)
 	perm=list(permutations(factors))
-	print(factors)
-	print(perm)
+	perm1=[]
+	for t in perm:
+		for ti in range(len(t)):
+			ind=factors.index(t[ti])
+			if ind+1<len(factors) and ti+1<len(t) and factors[ind+1]==t[ti+1]:
+				perm1.append(t)
+				break
+	for t in perm1:
+		perm.remove(t)
+	print(len(perm))
